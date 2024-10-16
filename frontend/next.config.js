ca821452275e68f409ea2dad0c66d05fd88cbe59
Stephanie Nguyen <tiennguyen.nltt@gmail.com>
2024-10-16 16:03:19 -0400

    updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_OPENAI_API_KEY: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   },
+  compiler: {
+    styledComponents: true,
+  },
 };
 
 module.exports = nextConfig;
